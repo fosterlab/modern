56e1753a2f78cc9c71e9a7eadb10b44cb702b672
Michael Skinnider <michaelskinnider@gmail.com>
2018-10-03 22:51:04 -0700

    Add option to bin by number of missing values to calculate z scores

--- R/detect_outliers.R
+++ R/detect_outliers.R
@@ -26,6 +26,9 @@
 #' Z scores, such that the matrix has a mean of zero and a standard deviation
 #' of one. If the matrix contains missing values, this scaling is performed for 
 #' each group of columns with equivalent numbers of missing values separately.
+#' Optionally, if there are many possible numbers of missing values, the z score
+#' can be calculated for approximately equal sized bins of missing value counts
+#' using the \code{bins} parameter.
 #' 
 #' @param mat a numeric matrix, with nodes (e.g., analytes such as genes, 
 #'   proteins, or metabolites) in columns, and samples in rows
@@ -36,26 +39,40 @@
 #' @param method the correlation coefficient to be computed; one of 
 #'   \code{"pearson"} (default), \code{"kendall"}, or \code{"spearman"}; 
 #'   can be abbreviated  
-#' 
+#' @param bins optionally, the number of bins into which to group nodes on the
+#'   basis of the number of observations
 #' @return a matrix with identical dimensions to the input matrix, containing
 #'   the autocorrelation Z score assigned to each non-missing observation 
 #'   
 #' @importFrom purrr %>%
+#' @importFrom mltools bin_data
 #' @export
 detect_outliers = function(mat, min_pairs = 10,
-                           method = c("pearson", "kendall", "spearman")) {
+                           method = c("pearson", "kendall", "spearman"),
+                           bins = NA) {
   method = match.arg(method)
   
   # calculate autocorrelations
   autocor = calculate_autocorrelation(mat, min_pairs, method)
 
-  # convert to Z scores, grouping nodes by # of missing values
-  z = autocor %>% 
+  # melt to a long data frame
+  long = autocor %>% 
     reshape2::melt(varnames = c("sample", "node")) %>%
     dplyr::group_by(node) %>%
     dplyr::mutate(n_obs = sum(!is.na(value))) %>%
-    dplyr::ungroup() %>%
-    dplyr::group_by(n_obs) %>%
+    ungroup()
+  
+  # if there are more bins than missing value counts, bin missing values
+  if (!is.na(bins) & dplyr::n_distinct(long$n_obs) > bins) {
+    long %<>% mutate(group = mltools::bin_data(
+      n_obs, bins = bins, binType = 'quantile'))
+  } else {
+    long %<>% mutate(group = n_obs)
+  }
+  
+  # convert to Z scores, grouping nodes by # of missing values
+  z = long %>% 
+    group_by(group) %>%
     dplyr::mutate(z = (value - mean(value, na.rm = T)) / 
                     sd(value, na.rm = T)) %>%
     dplyr::ungroup() %>%
